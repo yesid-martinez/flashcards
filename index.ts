b117f485aa9ab72048dcc3bb09822c79e1ca6a9c
Yesid M <wujibifan.hh@gmail.com>
2026-01-17 12:34:10 -0500

    feat: implement dynamic flashcard rendering

--- index.ts
+++ index.ts
@@ -1,8 +1,38 @@
+import data from './data/data.json' assert { type: 'json' };
+
+let isFlipped = false;
+
+const renderFlip = (isFlipped: boolean) => {
+    cardElement?.classList.toggle('flipped', isFlipped);
+};
+
 const cardElement = document.getElementById('card');
-if (!cardElement) {
-    console.warn('Card element not found');
-} else {
-        cardElement.addEventListener('click', () => {
-        cardElement.classList.toggle('flipped');
-        });
+cardElement?.addEventListener('click', () => {
+    isFlipped = !isFlipped;
+    renderFlip(isFlipped);
+});
+    
+const frontElement = cardElement?.querySelector('.front p');
+const backElement = cardElement?.querySelector('.back p');
+    
+let currentIndex = 0;
+
+const updateCardContent = () =>{
+    const currentContent = data[currentIndex];
+    if (currentContent && frontElement && backElement){
+        frontElement.textContent = currentContent.word;
+        backElement.textContent = currentContent.translation;
     };
+};
+
+const nextCardBtn = document.getElementById('nextBtn');
+nextCardBtn?.addEventListener('click', () => {
+    isFlipped = false;
+    renderFlip(isFlipped);
+
+    currentIndex = (currentIndex - 1 + data.length) % data.length;
+
+    updateCardContent();
+});
+
+updateCardContent();
